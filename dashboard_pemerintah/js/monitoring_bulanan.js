5d36257184f60b144516ea3b32b2e1df431d39d6
Muhammad Raihan Satrio Putra Pamungkas <62191088+aderio25@users.noreply.github.com>
2020-08-30 20:09:20 +0700

    Add files via upload

---
+++ dashboard_pemerintah/js/monitoring_bulanan.js
@@ -0,0 +1,31 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Bulan', 'Berhasil', 'Gagal'],
+    ['Jan',  100,      40],
+    ['Feb',  117,      46],
+    ['Mar',  360,       112],
+    ['Apr',  103,      54],
+    ['Mei',  250,      62],
+    ['Jun',  128,      54],
+  ]);
+
+  var options = {
+    title: 'Grafik Hasil Panen (Bulan)',
+    curveType: 'function',
+    legend: { position: 'bottom' },
+    width: 320,
+    height: 200,
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart_bulanan'));
+
+  chart.draw(data, options);
+}
+
+$(window).resize(function(){
+  drawChart();
+});
+ 
