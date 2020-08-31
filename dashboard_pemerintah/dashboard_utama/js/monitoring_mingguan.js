794ab412fbae899c567afba43d00bfe74795c0b4
Muhammad Raihan Satrio Putra Pamungkas <62191088+aderio25@users.noreply.github.com>
2020-08-31 18:03:40 +0700

    Update 1
    - Added logo on sidebar
    - Fix sidebar content

---
+++ dashboard_pemerintah/dashboard_utama/js/monitoring_mingguan.js
@@ -0,0 +1,28 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Minggu', 'Berhasil', 'Gagal'],
+    ['1',  56,      12],
+    ['2',  65,      12],
+    ['3',  66,       11],
+    ['4',  65,      12],
+  ]);
+
+  var options = {
+    title: 'Grafik Hasil Panen (Minggu)',
+    curveType: 'function',
+    legend: { position: 'bottom' },
+    width: 320,
+    height: 200,
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart_mingguan'));
+
+  chart.draw(data, options);
+}
+
+$(window).resize(function(){
+  drawChart();
+});
